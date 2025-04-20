095fd57560803f029b11e6f09529a14583ee973d
Atharva Bhawalkar <atharvabhawalkar147@gmail.com>
2025-04-20 15:07:28 +0530

    Middlewares and POST method

--- server_.js
+++ server_.js
@@ -7,49 +7,85 @@ const users = [
     {id: 3, name: 'Anakin Skywalker'}
 ]
 
+/* Logger Middelware */
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+/* Handler for creating new user */
+const createUserHandler = (req, res) => {
+
+    let body = '';
+
+    //Listen for data
+    req.on('data', (it) => {
+        body += it.toString();
+    });
+
+    req.on('end', () => {
+
+        const newUser = JSON.parse(body);
+        users.push(newUser);
+        res.statusCode = 201;
+        res.write(JSON.stringify(newUser));
+        res.end();
+        
+    });
+}
+
 const server = createServer((req, res) => {
 
-    if(req.url === '/api/users' && req.method === 'GET'){
+    logger(req, res, () => {
 
-        res
-            .setHeader('Content-Type', 'application/json')
-            .end(JSON.stringify(users));
-
-    }else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
-
-        const id = req.url.split('/')[3];
-        const user = users.find((it) => it.id === parseInt(id))
-
-        if(user){
+        if(req.url === '/api/users' && req.method === 'GET'){
 
             res
                 .setHeader('Content-Type', 'application/json')
-                .end(JSON.stringify(user));
+                .end(JSON.stringify(users));
+    
+        }else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
+    
+            const id = req.url.split('/')[3];
+            const user = users.find((it) => it.id === parseInt(id))
+    
+            if(user){
+    
+                res
+                    .setHeader('Content-Type', 'application/json')
+                    .end(JSON.stringify(user));
+    
+            }else{
+    
+                res
+                    .writeHead(404, {
+                        'Content-Type': 'text/plain',
+                    })
+                    .end('User not found');
+    
+                return;
+    
+            }
+            
+    
+        }else if(req.url === '/api/users' && req.method === 'POST'){
+
+            createUserHandler(req, res);
 
         }else{
-
+    
             res
                 .writeHead(404, {
                     'Content-Type': 'text/plain',
                 })
-                .end('User not found');
-
+                .end('Page not found');
+    
             return;
-
+    
         }
-        
 
-    }else{
-
-        res
-            .writeHead(404, {
-                'Content-Type': 'text/plain',
-            })
-            .end('Page not found');
-
-        return;
-
-    }
+    });
+    
 });
 
 server.listen(PORT, () => {
